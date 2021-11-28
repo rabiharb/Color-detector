8e1acfbadd5541d25bb08435ae58fe83587d9b36
rahih harb <rabiharb.ss@outlook.com>
2021-11-28 17:21:39 +0200

    last test

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, url_for
 from werkzeug.utils import redirect, secure_filename
 from color_detector import main as get_color_data
 import json
+import ast
 import os
 
 
@@ -9,8 +10,6 @@ UPLOAD_FOLDER = "static/user_images"
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 # --------------------- Globals
-# color_data = []
-# filename = ""
 
 
 app = Flask(__name__)
@@ -24,17 +23,16 @@ def allowed_file(filename):
 
 @ app.route("/image-colors", methods=["POST"])
 def get_image_colors():
-    global color_data, filename
     color_data = []
-    filename = ""
+    file_name = ""
     user_image_file = request.files["userImage"]
     if allowed_file(user_image_file.filename):
-        filename = secure_filename(user_image_file.filename)
+        file_name = secure_filename(user_image_file.filename)
         user_image_file.save(os.path.join(
-            app.config['UPLOAD_FOLDER'], filename))
+            app.config['UPLOAD_FOLDER'], file_name))
     color_data = get_color_data(os.path.join(
-        app.config['UPLOAD_FOLDER'], filename))
-    return redirect(url_for("color_extract_interface"))
+        app.config['UPLOAD_FOLDER'], file_name))
+    return redirect(url_for("color_extract_interface", file_name=file_name, color_data=color_data))
 
 
 @ app.route("/")
@@ -45,11 +43,17 @@ def home():
     return render_template("index.html", color_data=sample_color_data)
 
 
-@ app.route("/extract/", methods=["GET"])
+@ app.route("/extract/", methods=["GET", "POST"])
 def color_extract_interface():
-    global color_data
-    return render_template("color_extract.html", color_data=color_data, file_name=filename)
+    try:
+        color_data = ast.literal_eval(request.args.get("color_data"))
+        file_name = request.args.get("file_name")
+    except:
+        color_data = {}
+        file_name = ""
+    print(color_data, file_name, type(color_data))
+    return render_template("color_extract.html", color_data=color_data, file_name=file_name)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
