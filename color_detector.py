827add78c83943068f0747b783489d60c2f5337b
rahih harb <rabiharb.ss@outlook.com>
2021-11-28 13:12:14 +0200

    Initial Commit

---
+++ color_detector.py
@@ -0,0 +1,45 @@
+from collections import Counter
+from sklearn.cluster import KMeans
+import cv2
+
+
+# Image Resizing
+def pre_process(raw_img):
+    image = cv2.resize(raw_img, (900, 600), interpolation=cv2.INTER_AREA)
+    image = image.reshape(image.shape[0] * image.shape[1], 3)
+    return image
+
+
+# Converting rgb_values to hex
+def rgb_to_hex(rgb_color):
+    hex_color = "#"
+    for i in rgb_color:
+        i = int(i)
+        hex_color += ("{:02x}".format(i))
+    return hex_color
+
+
+def extract_colors(img):
+    clf = KMeans(n_clusters=5)
+    color_labels = clf.fit_predict(img)
+    center_colors = clf.cluster_centers_
+    counts = Counter(color_labels)
+    ordered_colors = [center_colors[i] for i in counts.keys()]
+    hex_colors = [rgb_to_hex(ordered_colors[i]) for i in counts.keys()]
+    rgb_colors = [str(tuple(int(j) for j in ordered_colors[i]))
+                  for i in counts.keys()]
+    color_count = [count for count in counts.values()]
+    image_color_data = {
+        "hex_colors": hex_colors,
+        "rgb_colors": rgb_colors,
+        "color_count": color_count
+    }
+    return image_color_data
+
+
+def main(img_path=""):
+    original_image = cv2.imread(
+        fr"{img_path}")
+    image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+    modified_image = pre_process(image)
+    return extract_colors(modified_image)
---
+++ main.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, render_template, url_for
+from werkzeug.utils import redirect, secure_filename
+from color_detector import main as get_color_data
+import json
+import os
+
+
+UPLOAD_FOLDER = r"Professional\Color-Detector-WebApp\static\user_images"
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+# --------------------- Globals
+color_data = []
+filename = ""
+
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@ app.route("/image-colors", methods=["POST"])
+def get_image_colors():
+    global color_data, filename
+    color_data = []
+    filename = ""
+    user_image_file = request.files["userImage"]
+    if allowed_file(user_image_file.filename):
+        filename = secure_filename(user_image_file.filename)
+        user_image_file.save(os.path.join(
+            app.config['UPLOAD_FOLDER'], filename))
+    color_data = get_color_data(os.path.join(
+        app.config['UPLOAD_FOLDER'], filename))
+    return redirect(url_for("color_extract_interface"))
+
+
+@ app.route("/")
+def home():
+    sample_color_data = {}
+    with open(r"Professional\Color-Detector-WebApp\json\samples_data.json", encoding="UTF-8") as f:
+        sample_color_data = json.load(fp=f)
+    return render_template("index.html", color_data=sample_color_data)
+
+
+@ app.route("/extract/", methods=["GET", "POST"])
+def color_extract_interface():
+    global color_data
+    return render_template("color_extract.html", color_data=color_data, file_name=filename)
+
+
+if __name__ == "__main__":
+    app.run()
